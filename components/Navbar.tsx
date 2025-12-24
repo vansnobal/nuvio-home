6c0c2bed274667f0ee07bfcdc760cd0674a828f5
vansnobal <97771752+vansnobal@users.noreply.github.com>
2025-12-24 20:21:18 +0900

    UI: Resize logo to 95% again (h-[72px] w-[231px])

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar: React.FC = () => {
             <img
               src={isDark ? "/logo-dark.png" : "/logo-light.png"}
               alt="Nuvio Note Logo"
-              className="h-[76px] w-[243px] object-contain object-left transition-all duration-300"
+              className="h-[72px] w-[231px] object-contain object-left transition-all duration-300"
             />
           </a>
         </div>
